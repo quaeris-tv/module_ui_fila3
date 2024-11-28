c1b9f0c6432956dfd2536148d3a98513ceb12d81
Davide Tricella <dadi.tricella@gmail.com>
2024-11-28 15:31:48 +0100

    up

---
+++ Resources/lang/it/images_gallery.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'fields' => 
+  array (
+    'version' => 
+    array (
+      'label' => 'version',
+    ),
+    'subtitle' => 
+    array (
+      'label' => 'subtitle',
+    ),
+    'title' => 
+    array (
+      'label' => 'title',
+    ),
+    'image' => 
+    array (
+      'label' => 'image',
+    ),
+  ),
+);
