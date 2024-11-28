5b4cde2dd0f788255eb293de71b042b361fb7595
Davide Tricella <dadi.tricella@gmail.com>
2024-11-28 15:31:48 +0100

    up

---
+++ Resources/lang/it/hero.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'fields' => 
+  array (
+    'link' => 
+    array (
+      'label' => 'link',
+    ),
+    'class' => 
+    array (
+      'label' => 'class',
+    ),
+    'label' => 
+    array (
+      'label' => 'label',
+    ),
+    'buttons' => 
+    array (
+      'label' => 'buttons',
+    ),
+    '_tpl' => 
+    array (
+      'label' => '_tpl',
+    ),
+    'background' => 
+    array (
+      'label' => 'background',
+    ),
+    'text' => 
+    array (
+      'label' => 'text',
+    ),
+  ),
+);
---
+++ Resources/lang/it/image.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'fields' => 
+  array (
+    'caption' => 
+    array (
+      'label' => 'caption',
+    ),
+    'alt' => 
+    array (
+      'label' => 'alt',
+    ),
+    'ratio' => 
+    array (
+      'label' => 'ratio',
+    ),
+    'url' => 
+    array (
+      'label' => 'url',
+    ),
+    'image' => 
+    array (
+      'label' => 'image',
+    ),
+  ),
+);
