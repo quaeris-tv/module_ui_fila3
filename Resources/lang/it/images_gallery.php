689bd71aa00546084e75f6863b61be831b291561
marco76tv <marco76tv@users.noreply.github.com>
2024-11-29 13:40:13 +0000

    Lint

--- Resources/lang/it/images_gallery.php
+++ Resources/lang/it/images_gallery.php
@@ -1,21 +1,20 @@
-<?php return array (
-  'fields' => 
-  array (
-    'version' => 
-    array (
-      'label' => 'version',
-    ),
-    'subtitle' => 
-    array (
-      'label' => 'subtitle',
-    ),
-    'title' => 
-    array (
-      'label' => 'title',
-    ),
-    'image' => 
-    array (
-      'label' => 'image',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'version' => [
+            'label' => 'version',
+        ],
+        'subtitle' => [
+            'label' => 'subtitle',
+        ],
+        'title' => [
+            'label' => 'title',
+        ],
+        'image' => [
+            'label' => 'image',
+        ],
+    ],
+];
