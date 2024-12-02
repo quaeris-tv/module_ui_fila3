c66d15e1a07027bb0a29cf7dfad956e65d1c7094
marco76tv <marco76tv@users.noreply.github.com>
2024-12-02 16:52:59 +0000

    Lint

--- Resources/lang/it/video_spatie.php
+++ Resources/lang/it/video_spatie.php
@@ -1,17 +1,17 @@
-<?php return array (
-  'fields' => 
-  array (
-    'img_uuid' => 
-    array (
-      'label' => 'img_uuid',
-    ),
-    'video' => 
-    array (
-      'label' => 'video',
-    ),
-    'caption' => 
-    array (
-      'label' => 'caption',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'img_uuid' => [
+            'label' => 'img_uuid',
+        ],
+        'video' => [
+            'label' => 'video',
+        ],
+        'caption' => [
+            'label' => 'caption',
+        ],
+    ],
+];
