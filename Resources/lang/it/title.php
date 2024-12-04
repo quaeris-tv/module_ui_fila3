e12e9f1018e3ad2a0a89401abc75908c30addce0
marco76tv <marco76tv@users.noreply.github.com>
2024-12-04 16:46:51 +0000

    Lint

--- Resources/lang/it/title.php
+++ Resources/lang/it/title.php
@@ -1,21 +1,20 @@
-<?php return array (
-  'fields' => 
-  array (
-    'text' => 
-    array (
-      'label' => 'Testo',
-    ),
-    'level' => 
-    array (
-      'label' => 'Grandezza',
-    ),
-    '_tpl' => 
-    array (
-      'label' => '_tpl',
-    ),
-    'view' => 
-    array (
-      'label' => 'view',
-    ),
-  ),
-);
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'text' => [
+            'label' => 'Testo',
+        ],
+        'level' => [
+            'label' => 'Grandezza',
+        ],
+        '_tpl' => [
+            'label' => '_tpl',
+        ],
+        'view' => [
+            'label' => 'view',
+        ],
+    ],
+];
