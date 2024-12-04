c6e3181909bd3aa5dfce9ce961bc49d51de9c7bc
Malebestia <vair81@gmail.com>
2024-12-04 17:45:17 +0100

    feat: updated paragraph component to use new version prediction logic

--- Filament/Blocks/Paragraph.php
+++ Filament/Blocks/Paragraph.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Blocks;
 
-use Filament\Forms\Components\Builder\Block;
-use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
-use Modules\Xot\Actions\View\GetViewsSiblingsAndSelfAction;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Builder\Block;
+// use Modules\Xot\Actions\View\GetViewsSiblingsAndSelfAction;
+use Modules\UI\Filament\Forms\Components\RadioImage;
+use Modules\Xot\Actions\Filament\Block\GetViewBlocksOptionsByTypeAction;
 
 class Paragraph
 {
@@ -16,17 +18,21 @@ class Paragraph
         string $name = 'paragraph',
         string $context = 'form',
     ): Block {
-        $view = 'ui::components.blocks.paragraph.v1';
-        $views = app(GetViewsSiblingsAndSelfAction::class)->execute($view);
+        // $view = 'ui::components.blocks.paragraph.v1';
+        // $views = app(GetViewsSiblingsAndSelfAction::class)->execute($view);
+
+        $options = app(GetViewBlocksOptionsByTypeAction::class)
+            ->execute('paragraph', true);
 
         return Block::make($name)
             ->schema(
                 [
                     TextInput::make('title'),
                     RichEditor::make('text'),
-                    Select::make('_tpl')
-                        
-                        ->options($views),
+                    Select::make('view')
+                        ->options($options),
+                    RadioImage::make('view')
+                        ->options($options),
                 ]
             );
     }
