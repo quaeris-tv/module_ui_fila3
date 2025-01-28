e4c57961cd0a347dbfc55322bc2ded6badaddfb0
marco76tv <marco.sottana@gmail.com>
2025-01-28 17:29:10 +0100

    up

--- config/config.php
+++ config/config.php
@@ -2,4 +2,19 @@
 
 declare(strict_types=1);
 
-return [];
+return [
+    'name' => 'UI',
+    'description' => 'Modulo per la gestione dell\'interfaccia utente e componenti',
+    'icon' => 'heroicon-o-squares-2x2',
+    'navigation' => [
+        'enabled' => true,
+        'sort' => 90,
+    ],
+    'routes' => [
+        'enabled' => true,
+        'middleware' => ['web', 'auth'],
+    ],
+    'providers' => [
+        'Modules\\UI\\Providers\\UIServiceProvider',
+    ],
+];
