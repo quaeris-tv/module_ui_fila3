39b6dc7603e2c4dda9fdbee3ceca654a24001f66
marco76tv <marco.sottana@gmail.com>
2025-01-28 09:08:50 +0100

    up

--- config/config.php
+++ config/config.php
@@ -2,9 +2,4 @@
 
 declare(strict_types=1);
 
-return [
-    'name' => 'UI',
-    'navigation_sort' => 1,
-    // 'icon' => 'heroicon-o-cog', // icon on dashboard
-    'icon' => 'fas-theater-masks',
-];
+return [];
