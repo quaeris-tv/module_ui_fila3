52fba9ee3d34f8208c68ab6a589afc0f22ffd930
marco76tv <marco.sottana@gmail.com>
2025-02-12 14:23:34 +0100

    up

---
+++ lang/it/ui.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'navigation' => [
+        'name' => 'Interfaccia',
+        'plural' => 'Interfacce',
+        'group' => 'Sistema',
+        'label' => 'ui',
+        'sort' => 70,
+        'icon' => 'heroicon-o-squares-2x2',
+    ],
+];
