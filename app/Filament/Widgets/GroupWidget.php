e6ff7386c770f75dc86db823149e26689b3a3001
marco76tv <marco.sottana@gmail.com>
2025-02-26 15:45:31 +0100

    Auto update submodule

--- app/Filament/Widgets/GroupWidget.php
+++ app/Filament/Widgets/GroupWidget.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace Modules\UI\Filament\Widgets;
 
-use Filament\Widgets\Widget as BaseWidget;
 use Modules\Xot\Filament\Widgets\XotBaseWidget;
-use Filament\Widgets\Concerns\InteractsWithPageFilters;
 
 class GroupWidget extends XotBaseWidget
 {
     public array $widgets = [];
+
     protected static ?string $pollingInterval = null;
-    
 }
